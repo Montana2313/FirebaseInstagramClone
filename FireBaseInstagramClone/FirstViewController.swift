f7df5af702cf5431daf4cdb892df085b9a11b1bf
Özgür Elmasli <ozgur968@gmail.com>
2019-03-09 23:52:45 +0300

    Initial Commit

---
+++ FireBaseInstagramClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FireBaseInstagramClone
+//
+//  Created by Özgür  Elmaslı on 9.03.2019.
+//  Copyright © 2019 Özgür  Elmaslı. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FireBaseInstagramClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FireBaseInstagramClone
+//
+//  Created by Özgür  Elmaslı on 9.03.2019.
+//  Copyright © 2019 Özgür  Elmaslı. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
